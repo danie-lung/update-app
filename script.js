b2a593a976bcf9f60374c1e26dde374ed8d2cd57
danie-lung <danieltajee@gmail.com>
2026-02-04 12:18:57 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
+function showToast(message, type) {
+
+  const toastEl = document.getElementById('liveToast');
+  const toastBody = document.getElementById('toastBody');
+
+  toastBody.innerText = message;
+
+  // Reset class
+  toastEl.className = "toast align-items-center border-0";
+
+  if (type === "success") {
+    toastEl.classList.add("text-bg-success");
+  } else if (type === "error") {
+    toastEl.classList.add("text-bg-danger");
+  } else {
+    toastEl.classList.add("text-bg-primary");
+  }
+
+  const toast = new bootstrap.Toast(toastEl, {
+    delay: 3000
+  });
+
+  toast.show();
+}
+
 function cekKode(){
 
   let kode = document.getElementById("kode").value;
   let app = document.getElementById("app").value;
 
-  let msg = document.getElementById("msg");
   let btn = document.getElementById("btnDownload");
   let loader = document.getElementById("loader");
   let btnText = document.getElementById("btnText");
 
   if(app === ""){
-    msg.className = "text-danger fw-semibold mt-3";
-    msg.innerText = "Pilih aplikasi terlebih dahulu";
+    showToast("Pilih aplikasi terlebih dahulu", "error");
     return;
   }
 
   if(kode === ""){
-    msg.className = "text-danger fw-semibold mt-3";
-    msg.innerText = "Masukkan kode download";
+    showToast("Masukkan kode download", "error");
     return;
   }
 
@@ -24,17 +46,14 @@ function cekKode(){
   btn.disabled = true;
   loader.classList.remove("d-none");
   btnText.innerText = "Memproses...";
-  msg.innerText = "";
 
-  // Kirim ke API GAS
-  fetch("https://script.google.com/macros/s/AKfycbxD1UP6VgYXftX7ChF7f41w6-hUNdLH9VcSPV1RSiaLwrbtEinZk65Lqppz8qygMqzGPQ/exec?kode=" + kode + "&app=" + app)
+  fetch("https://script.google.com/macros/s/AKfycbxR4BkXbqeN2cBOh5klg2fCzMRzkvkAgdSb8acMnPtZSX8VQCRm3QwZqsmGiNQOW1QUdw/exec?kode=" + kode + "&app=" + app)
   .then(res => res.json())
   .then(data => {
 
     if(data.status === "ok"){
 
-      msg.className = "text-success fw-semibold mt-3";
-      msg.innerText = "Kode valid ✅ Mengunduh...";
+      showToast("Kode valid, sedang mengunduh...", "success");
 
       setTimeout(() => {
         window.location.href = data.link;
@@ -42,10 +61,8 @@ function cekKode(){
 
     } else {
 
-      msg.className = "text-danger fw-semibold mt-3";
-      msg.innerText = data.msg;
+      showToast(data.msg, "error");
 
-      // Unlock UI
       btn.disabled = false;
       loader.classList.add("d-none");
       btnText.innerText = "Download";
@@ -55,8 +72,7 @@ function cekKode(){
   })
   .catch(() => {
 
-    msg.className = "text-danger fw-semibold mt-3";
-    msg.innerText = "Server error";
+    showToast("Server error", "error");
 
     btn.disabled = false;
     loader.classList.add("d-none");
